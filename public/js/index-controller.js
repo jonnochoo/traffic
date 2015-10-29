62900cddca77744ba2088c2a5297b4ec06ac2d38
Jonathon Choo <jonno.choo@gmail.com>
2015-10-29 23:03:22 +1100

    Added ability to refresh

--- public/js/index-controller.js
+++ public/js/index-controller.js
@@ -4,13 +4,18 @@ Controller.$inject = ['$http', '$interval', 'trafficService'];
 
 function Controller($http, $interval, trafficService) {
   var vm = this;
+  vm.reload = reload;
 
   (function constructor() {
+    reload();
+    $interval(refresh, 1000);
+  })();
+
+  function reload() {
     trafficService.getAlerts().then(result => {
       vm.results = result;
     });
-    $interval(refresh, 1000);
-  })();
+  }
 
   function refresh() {
     _.each(vm.results, item => { item.refresh(); });
